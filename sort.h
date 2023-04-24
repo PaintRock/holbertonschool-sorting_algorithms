268e778db399244301428f1b0e5409156a9197a7
adel <6126.holbertonstudents.com>
2023-04-24 09:02:26 -0700

    be done

--- sort.h
+++ sort.h
@@ -24,4 +24,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void partition(int *array, size_t size, int lo, int hi);
+void swap(int *array, size_t size, int idx, int wall);
+void partition(int *array, size_t size, int lo, int hi);
 #endif
